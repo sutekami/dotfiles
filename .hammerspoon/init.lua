f69928fbcdf2e169dfd811004d9ac6a1b925e9ef
sutekami <115330856+sutekami@users.noreply.github.com>
2026-08-10 10:15:25 +0900

    update: hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -33,7 +33,9 @@ launchOrFocus({ "cmd", "ctrl" }, "s", "Slack")
 
 -- Command + Control + E で Zed を起動またはフォーカス
 -- 2026/07/10 Zed にしてみる
--- launchOrFocus({ "cmd", "ctrl" }, "e", "Zed")
+launchOrFocus({ "cmd", "ctrl" }, "e", "Zed")
+
+launchOrFocus({ "cmd", "ctrl" }, "i", "Xcode")
 
 -- Command + Control + L で Linear を起動またはフォーカス
 launchOrFocus({ "cmd", "ctrl" }, "p", "Linear")
